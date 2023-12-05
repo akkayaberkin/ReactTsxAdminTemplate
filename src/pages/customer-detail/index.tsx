59518a8f383696265267f13301ac938927c71fc1
berkin <berkin-akkaya@hotmail.com>
2023-12-05 20:28:19 +0300

    customer gelistirmesi basladi

---
+++ src/pages/customer-detail/index.tsx
@@ -0,0 +1,242 @@
+// ** React Imports
+import { SyntheticEvent, useEffect, useState,ChangeEvent, ElementType } from 'react'
+import { useRouter } from 'next/router'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import TabList from '@mui/lab/TabList'
+import TabPanel from '@mui/lab/TabPanel'
+import TabContext from '@mui/lab/TabContext'
+import { styled } from '@mui/material/styles'
+import MuiTab, { TabProps } from '@mui/material/Tab'
+
+// ** Icons Imports
+import AccountOutline from 'mdi-material-ui/AccountOutline'
+import LockOpenOutline from 'mdi-material-ui/LockOpenOutline'
+import InformationOutline from 'mdi-material-ui/InformationOutline'
+
+// ** Demo Tabs Imports
+import TabInfo from 'src/views/account-settings/TabInfo'
+import TabAccount from 'src/views/account-settings/TabAccount'
+import TabSecurity from 'src/views/account-settings/TabSecurity'
+
+// ** Third Party Styles Imports
+import 'react-datepicker/dist/react-datepicker.css'
+import { TextBox } from 'mdi-material-ui'
+import { Alert, AlertTitle, Button, CardContent, FormControl, Grid, IconButton, InputLabel, Link, MenuItem, Select, TextField, Typography } from '@mui/material'
+import { getData } from 'src/api'
+import { Close } from '@material-ui/icons'
+import { ButtonProps } from '@mui/material/Button'
+
+const Tab = styled(MuiTab)<TabProps>(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    minWidth: 100
+  },
+  [theme.breakpoints.down('sm')]: {
+    minWidth: 67
+  }
+}))
+
+const TabName = styled('span')(({ theme }) => ({
+  lineHeight: 1.71,
+  fontSize: '0.875rem',
+  marginLeft: theme.spacing(2.4),
+  [theme.breakpoints.down('md')]: {
+    display: 'none'
+  }
+}))
+const ImgStyled = styled('img')(({ theme }) => ({
+  width: 120,
+  height: 120,
+  marginRight: theme.spacing(6.25),
+  borderRadius: theme.shape.borderRadius
+}))
+
+const ButtonStyled = styled(Button)<ButtonProps & { component?: ElementType; htmlFor?: string }>(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    textAlign: 'center'
+  }
+}))
+
+const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
+  marginLeft: theme.spacing(4.5),
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    marginLeft: 0,
+    textAlign: 'center',
+    marginTop: theme.spacing(4)
+  }
+}))
+
+const CustomerDetail = () => {
+  const [data, setData] = useState<any>()
+
+  useEffect(() => {
+    getCustomerDetail(username)
+  }, []);
+  const getCustomerDetail = async (username:any) => {
+    await getData('/customer/GetByUserName/?username='+username).then((res:any) => res.data).then((res:any) => {
+      debugger
+      setData(res);
+      });
+  }
+  const router = useRouter()
+  const queryParams = new URLSearchParams(router.asPath.split(/\?/)[1])
+  const username = queryParams.get('username')
+
+  const [value, setValue] = useState<string>('account')
+  const [imgSrc, setImgSrc] = useState<string>('/images/avatars/1.png')
+
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
+    setValue(newValue)
+  }
+  const onChange = (file: ChangeEvent) => {
+    const reader = new FileReader()
+    const { files } = file.target as HTMLInputElement
+    if (files && files.length !== 0) {
+      reader.onload = () => setImgSrc(reader.result as string)
+
+      reader.readAsDataURL(files[0])
+    }
+  }
+  const [openAlert, setOpenAlert] = useState<boolean>(true)
+
+  return (
+    <>
+      {username && <h1>{username}</h1>}
+      <h1>Customer Detail</h1>
+      {!username ? (
+        <div style={{display:"flex",flexDirection:"column",justifyContent:"center"}}>
+        <TextField label="Username">
+          {username}
+        </TextField>
+        </div>
+      ) : (
+        data && (
+        <Card>
+          <TabContext value={value}>
+            <TabList
+              onChange={handleChange}
+              aria-label='account-settings tabs'
+              sx={{ borderBottom: theme => `1px solid ${theme.palette.divider}` }}
+            >
+              <Tab
+                value='account'
+                label={
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <AccountOutline />
+                    <TabName>Müşteri bilgileri</TabName>
+                  </Box>
+                }
+              />
+            </TabList>
+
+            <CardContent>
+      <form>
+        <Grid container spacing={7}>
+          <Grid item xs={12} sx={{ marginTop: 4.8, marginBottom: 3 }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <ImgStyled src={imgSrc} alt='Profile Pic' />
+              <Box>
+                <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
+                  Upload New Photo
+                  <input
+                    hidden
+                    type='file'
+                    onChange={onChange}
+                    accept='image/png, image/jpeg'
+                    id='account-settings-upload-image'
+                  />
+                </ButtonStyled>
+                <ResetButtonStyled color='error' variant='outlined' onClick={() => setImgSrc('/images/avatars/1.png')}>
+                  Reset
+                </ResetButtonStyled>
+                <Typography variant='body2' sx={{ marginTop: 5 }}>
+                  Allowed PNG or JPEG. Max size of 800K.
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField fullWidth label='Username' placeholder={data.username} defaultValue={data.username} />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField fullWidth label='Name' placeholder='John Doe' defaultValue='John Doe' />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              type='email'
+              label='Email'
+              placeholder={data.email}
+              defaultValue={data.email}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Role</InputLabel>
+              <Select label='Role' defaultValue='admin'>
+                <MenuItem value='admin'>Admin</MenuItem>
+                <MenuItem value='author'>Author</MenuItem>
+                <MenuItem value='editor'>Editor</MenuItem>
+                <MenuItem value='maintainer'>Maintainer</MenuItem>
+                <MenuItem value='subscriber'>Subscriber</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Status</InputLabel>
+              <Select label='Status' defaultValue='active'>
+                <MenuItem value='active'>Active</MenuItem>
+                <MenuItem value='inactive'>Inactive</MenuItem>
+                <MenuItem value='pending'>Pending</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField fullWidth label='Üyelik' placeholder={data.createdDate} defaultValue={data.createdDate} />
+          </Grid>
+
+          {openAlert ? (
+            <Grid item xs={12} sx={{ mb: 3 }}>
+              <Alert
+                severity='warning'
+                sx={{ '& a': { fontWeight: 400 } }}
+                action={
+                  <IconButton size='small' color='inherit' aria-label='close' onClick={() => setOpenAlert(false)}>
+                    <Close fontSize='inherit' />
+                  </IconButton>
+                }
+              >
+                <AlertTitle>Lütfen e posta adresinizi doğrulayın</AlertTitle>
+                <Link href='www.allahaemanet.com' target='_blank' onClick={(e: SyntheticEvent) => e.preventDefault()}>
+                  Resend Confirmation
+                </Link>
+              </Alert>
+            </Grid>
+          ) : null}
+
+          <Grid item xs={12}>
+            <Button variant='contained' sx={{ marginRight: 3.5 }}>
+              Save Changes
+            </Button>
+            <Button type='reset' variant='outlined' color='secondary'>
+              Reset
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </CardContent>
+            
+          </TabContext>
+        </Card>
+        )  
+      )}
+    </>
+  )
+}
+export default CustomerDetail
